26f0ca00c9cad7df4031dccc80aa8f71b3675103
Skyler Cheng <skyler.cheng.hello@gmail.com>
2021-06-15 16:52:57 +0800

    no message

---
+++ components/Header.vue
@@ -0,0 +1,19 @@
+<template>
+    <header id="header">
+        <div class="inner">
+
+            <!-- Logo -->
+            <nuxt-link to="/" class="logo">
+                <span class="symbol"><img src="~/assets/images/logo.svg" alt="" /></span><span class="title">Skyler 覆歷</span>
+            </nuxt-link>
+
+            <!-- Nav -->
+            <nav>
+                <ul>
+                <li><a href="#menu">Menu</a></li>
+                </ul>
+            </nav>
+
+        </div>
+    </header>
+</template>
---
+++ components/Menu.vue
@@ -0,0 +1,12 @@
+<template>
+    <nav id="menu">
+        <h2>Menu</h2>
+        <ul>
+            <li><nuxt-link to="/">Home</nuxt-link></li>
+            <li><nuxt-link to="/generic">Ipsum veroeros</nuxt-link></li>
+            <li><nuxt-link to="/generic">Tempus etiam</nuxt-link></li>
+            <li><nuxt-link to="/generic">Consequat dolor</nuxt-link></li>
+            <li><nuxt-link to="/elements">Elements</nuxt-link></li>
+        </ul>
+    </nav>
+</template>
